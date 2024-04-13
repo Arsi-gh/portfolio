acef7e24b713401f594f978ca0c6853ec9b43d24
Arsi <arsalanghoochani15@gmail.com>
2024-04-13 12:45:38 -0700

    v-0.2.5

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Faq from "@/components/home-ui/FAQ";
 import AboutMe from "@/components/home-ui/aboutMe";
 import CommentWrapper from "@/components/home-ui/comments/commentWrapper";
+import Faq from "@/components/home-ui/faq";
 import MoreProject from "@/components/home-ui/moreProject";
 import ProjectWrapper from "@/components/home-ui/projects/projectWrapper";
 import Services from "@/components/home-ui/services";
